4b51ab8e51a5ef3b58d1cae2857a76fb3953f84e
jackson <jackson.mangallay@hotmail.com>
2021-10-30 22:58:12 +0800

    package update + ts fixes

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -1,9 +1,9 @@
-import { boot } from 'quasar/wrappers';
-import axios, { AxiosInstance } from 'axios';
+import { boot } from 'quasar/wrappers'
+import axios, { AxiosInstance } from 'axios'
 
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
-    $axios: AxiosInstance;
+    $axios: AxiosInstance
   }
 }
 
@@ -13,7 +13,7 @@ declare module '@vue/runtime-core' {
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ 
+const api = axios.create({
   baseURL: process.env.API,
   headers: {
     'Content-Type': 'application/json',
@@ -21,19 +21,19 @@ const api = axios.create({
     'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
     xsrfCookieName: 'XSRF-TOKEN',
     xsrfHeaderName: 'X-XSRF-TOKEN'
-  },
-});
+  }
+})
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
 
-  app.config.globalProperties.$axios = axios;
+  app.config.globalProperties.$axios = axios
   // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
   //       so you won't necessarily have to import axios in each vue file
 
-  app.config.globalProperties.$api = api;
+  app.config.globalProperties.$api = api
   // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
   //       so you can easily perform requests against your app's API
-});
+})
 
-export { api };
+export { api }
